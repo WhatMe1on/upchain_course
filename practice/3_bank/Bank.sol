53cc80586be31fa8d65d0a76ade0c40a7dfc8341
Whatmelon <ccacrop.code@gmail.com>
2024-12-16 07:54:39 +0800

    fix the top3 deposit user state

--- practice/3_bank/Bank.sol
+++ practice/3_bank/Bank.sol
@@ -2,14 +2,20 @@
 pragma solidity ^0.8.18;
 
 contract Bank {
-    mapping(address => uint) s_balanceSheep;
+    BalanceSheet[3] public balanceSheet;
+
     address public s_owner;
-    uint public balance;
-    uint public counter;
+    uint256 public balance;
+    uint256 public counter;
 
     error Bank__NotOwner();
     error Bank__withdraw2Failed();
     error Bank__withdraw3Failed();
+
+    struct BalanceSheet {
+        address userAddress;
+        uint256 balance;
+    }
 
     modifier M_onlyOwner() {
         if (msg.sender != s_owner) {
@@ -43,9 +49,7 @@ contract Bank {
     }
 
     function withdraw3() public M_onlyOwner {
-        (bool success, ) = (payable(msg.sender)).call{
-            value: address(this).balance
-        }("");
+        (bool success,) = (payable(msg.sender)).call{value: address(this).balance}("");
         if (!success) {
             revert Bank__withdraw3Failed();
         }
@@ -53,11 +57,33 @@ contract Bank {
     }
 
     function deposit() public payable {
-        s_balanceSheep[msg.sender] = msg.value;
+        insertUser(msg.sender, msg.value);
         changeBalance();
     }
 
     function changeBalance() private {
         balance = address(this).balance;
+    }
+
+    function insertUser(address _address, uint256 _balance) private {
+        uint8 minIndex = 0;
+        uint256 minBalance = 0xffffffffffffffff;
+
+        for (uint8 index = 0 ; index < balanceSheet.length; index++) {
+            uint256 tmp = balanceSheet[index].balance;
+            address storedAddress = balanceSheet[index].userAddress;
+            if (storedAddress == _address) {
+                balanceSheet[index].balance = tmp + _balance;
+                return;
+            }
+            if (minBalance > tmp) {
+                minIndex = index;
+                minBalance = tmp;
+            }
+        }
+
+        if (minBalance < _balance) {
+            balanceSheet[minIndex] = BalanceSheet({userAddress: _address, balance: _balance});
+        }
     }
 }
