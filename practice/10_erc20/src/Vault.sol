e750e73c94fe5f2b034e8c1eb4309e5f20a924f1
Whatmelon <ccacrop.code@gmail.com>
2024-12-18 11:17:06 +0800

    Default commit message

---
+++ practice/10_erc20/src/Vault.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {TokenRecipient} from "./VaultERC20.sol";
+
+contract Vault is TokenRecipient {
+    error callerNotTokenAddress();
+
+    address immutable s_tokenAddress;
+    mapping(address => uint256) s_balanceMap;
+
+    constructor(address _tokenAddress) {
+        s_tokenAddress = _tokenAddress;
+    }
+
+    function depositToken(address user, uint256 amount) external returns (bool) {
+        bool returnFlag = false;
+        if (msg.sender == s_tokenAddress) {
+            s_balanceMap[user] += amount;
+            returnFlag = true;
+        } else {
+            revert callerNotTokenAddress();
+        }
+        return returnFlag;
+    }
+
+    function withdrawToken(address user, uint256 amount) external returns (bool) {
+        bool returnFlag = false;
+        if (msg.sender == s_tokenAddress) {
+            s_balanceMap[user] -= amount;
+            returnFlag = true;
+        } else {
+            revert callerNotTokenAddress();
+        }
+        return returnFlag;
+    }
+
+    function getBalance(address user) public view returns (uint) {
+        return s_balanceMap[user];
+    }
+
+    function getBalance() external view returns (uint) {
+        return s_balanceMap[msg.sender];
+    }
+}
---
+++ practice/10_erc20/src/VaultERC20.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+interface TokenRecipient {
+    function depositToken(address user, uint256 amount) external returns (bool);
+    function withdrawToken(address user, uint256 amount) external returns (bool);
+}
+
+contract VaultERC20 is ERC20 {
+    using Address for address;
+
+    error VaultERC20__NoTokensReceived();
+
+    constructor() ERC20("VaultCoin", "VaC") {
+        _mint(msg.sender, 100000);
+    }
+
+    //Actual environment don's exist this function, token was transfer by ERC20's transfer() or transferFrom()
+    function getToken() external {
+        _mint(msg.sender, 100000);
+    }
+
+    function deposit(address s_receipt, uint256 amount) external returns (bool) {
+        //Add this function can let allowance reduce by the amount
+        // _spendAllowance(msg.sender, s_receipt, amount);
+        _transfer(msg.sender, s_receipt, amount);
+
+        bool rv = TokenRecipient(s_receipt).depositToken(msg.sender, amount);
+        if (!rv) {
+            revert VaultERC20__NoTokensReceived();
+        }
+
+        return true;
+    }
+
+    function withdraw(address s_receipt, uint256 amount) external returns (bool) {
+        //Add this function can let allowance reduce by the amount
+        // _spendAllowance(msg.sender, s_receipt, amount);
+        _transfer(s_receipt, msg.sender, amount);
+
+        bool rv = TokenRecipient(s_receipt).withdrawToken(msg.sender, amount);
+        if (!rv) {
+            revert VaultERC20__NoTokensReceived();
+        }
+
+        return true;
+    }
+}
