cfe8c7fd8c818dd0a711ead34bb8d8037a52c3bc
Whatmelon <ccacrop.code@gmail.com>
2024-12-28 21:03:08 +0800

    add translate to git add&commit&push shell script

--- gitACP.sh
+++ gitACP.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # 设置默认的 commit message
-DEFAULT_MSG="Default commit message"
+DEFAULT_MSG="默认提交"
 
 # 使用传入的参数作为 commit message，否则使用默认值
 COMMIT_MSG="${1:-$DEFAULT_MSG}"
+
+english_text=$(trans -b :en "$COMMIT_MSG")
+
+# 拼接中文和英文
+COMMIT_MSG="$COMMIT_MSG - $english_text"
 
 # 执行 git 操作
 git add . && \
